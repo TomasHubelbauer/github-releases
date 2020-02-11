5987627b73ca89ffdceff8cffe99f07ccbcea3b1
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-02-11 21:24:05 +0100

    Implement the idea

--- README.md
+++ README.md
@@ -16,4 +16,21 @@ apply.
 
 ## To-Do
 
-### Use the `github-api` project of mine to hook this up
+### Pull all the repositories first and look at ones with known releases first
+
+This will maximize the opportunity to find new releases as repositories which
+already have releases are more likely to have new ones than the ones that don't
+have any releases yet.
+
+### Group the repositories into buckets and run different buckets on each run
+
+Maybe group by the first name or something because as it stands this might blow
+all the way through even the authenticated rate limits and if not, eat up a
+significant chunk which chokes subsequent GitHub API users on the IP.
+
+### Implement a blocklist
+
+There are ridiculous things like the web platform tests (WPT) or Jackett which
+have just loads of releases it is worth ignoring them for the sake of saving the
+rate limit. Or maybe run them conditionally last, if there is still enough of 
+the limit left for us to be able to blow it.
---
+++ index.js
@@ -0,0 +1,69 @@
+const github = require('github-api');
+const fs = require('fs-extra');
+let email;
+let headers;
+try {
+  email = require('../self-email');
+  headers = require('../self-email/headers');
+}
+catch (error) {
+  // Ignore the mailer missing on this system
+}
+
+// Crash the process on an unhandled promise rejection
+process.on('unhandledRejection', error => { throw error; });
+
+module.exports = async function () {
+  const token = process.argv[2] || process.env.GITHUB_TOKEN;
+  const repositories = token
+    ? github.getUserStarred({ token })
+    : github.getUsersUserStarred('TomasHubelbauer')
+    ;
+
+  const emailLines = [];
+  for await (const repository of repositories) {
+    /** @type {{ id: number; name: string; url: string; }[]} */
+    const releases = [];
+    for await (const release of github.getReposOwnerRepoReleases(repository.full_name, { token })) {
+      releases.push({ id: release.id, name: release.name, url: release.html_url });
+    }
+
+    console.log(repository.full_name, releases.length, 'releases');
+    const path = `data/${repository.full_name}.json`;
+
+    /** @type {{ id: number; name: string; url: string; }[]} */
+    let knownReleases = [];
+    try {
+      knownReleases = await fs.readJson(path);
+    }
+    catch (error) {
+      // Ignore no releases being known yet
+    }
+
+    const newRepoReleases = releases.filter(r => !knownReleases.find(r2 => r2.id === r.id));
+    if (newRepoReleases.length > 0) {
+      emailLines.push(`<p><b>${repository.full_name}</b></p>`);
+      emailLines.push('<ul>');
+      for (const release of newRepoReleases) {
+        emailLines.push(`<li><a href="${release.url}">${release.name}</a></li>`);
+      }
+
+      emailLines.push('</ul>');
+    }
+
+    await fs.ensureFile(path);
+    await fs.writeJson(path, releases, { spaces: 2 });
+  }
+
+  if (emailLines.length > 0 && email) {
+    const repositories = emailLines.filter(l => l.startsWith('<p><b>')).length;
+    const releases = emailLines.filter(l => l.startsWith('<li>')).length;
+    await email(
+      headers('GitHub Releases', `${releases} new releases across ${repositories} repositories`),
+      `<p>There are ${releases} across ${repositories} repositories:</p>`,
+      ...emailLines
+    );
+  }
+};
+
+module.exports = module.exports();
